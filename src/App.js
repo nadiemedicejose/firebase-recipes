b0c0fee2656534f2c064d1a16f6a27c92dec7d38
Jose Estrada <jose.estrada@sonorasoft.com>
2022-07-19 12:12:23 -0700

    AddEditRecipeForm integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import FirebaseFirestoreService from './FirebaseFirestoreService';
 import FirebaseAuthService from './FirebaseAuthService';
 import LoginForm from './components/LoginForm';
+import AddEditRecipeForm from './components/AddEditRecipeForm';
 
 import './App.css';
 
@@ -8,6 +10,19 @@ export const App = () => {
 	const [user, setUser] = useState(null);
 
 	FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+	async function handleAddRecipe(newRecipe) {
+		try {
+			const response = await FirebaseFirestoreService.createDocument('recipes', newRecipe);
+
+			// todo: fetch new recipes from firestore
+
+			alert(`Successfully created a recipe with an Id = ${response.id}`);
+
+		} catch (e) {
+			alert(e.message);
+		}
+	}
 
 	return (
 		<div className="App">
